6bfa0d1c69fc5b5315421ccff4bec51d39e4cfed
valentinabobetic <valentina.bobetic@gmail.com>
2014-05-29 20:37:23 +0200

    projekt i forme prijava i registracija

---
+++ Projekt/eNarudžba/eNarudžba/eNarudžba/Forme/RegistracijaForma.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace eNarudžba.Forme
+{
+    partial class RegistracijaForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIme = new System.Windows.Forms.Label();
+            this.lblPrezime = new System.Windows.Forms.Label();
+            this.lblKorisnickoIme = new System.Windows.Forms.Label();
+            this.lblLozinka = new System.Windows.Forms.Label();
+            this.lblPotvrdaLozinke = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblAdresa = new System.Windows.Forms.Label();
+            this.lblStudent = new System.Windows.Forms.Label();
+            this.txtBoxIme = new System.Windows.Forms.TextBox();
+            this.txtBoxPrezime = new System.Windows.Forms.TextBox();
+            this.txtBoxRegKorIme = new System.Windows.Forms.TextBox();
+            this.txtBoxRegLozinka = new System.Windows.Forms.TextBox();
+            this.txtBoxPotvrdaLozinke = new System.Windows.Forms.TextBox();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.txtBoxAdresa = new System.Windows.Forms.TextBox();
+            this.radioBtnDa = new System.Windows.Forms.RadioButton();
+            this.radioBtnNe = new System.Windows.Forms.RadioButton();
+            this.btnRegRegistracija = new System.Windows.Forms.Button();
+            this.lblOIB = new System.Windows.Forms.Label();
+            this.txtBoxOIB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblIme
+            // 
+            this.lblIme.AutoSize = true;
+            this.lblIme.Location = new System.Drawing.Point(23, 29);
+            this.lblIme.Name = "lblIme";
+            this.lblIme.Size = new System.Drawing.Size(24, 13);
+            this.lblIme.TabIndex = 0;
+            this.lblIme.Text = "Ime";
+            // 
+            // lblPrezime
+            // 
+            this.lblPrezime.AutoSize = true;
+            this.lblPrezime.Location = new System.Drawing.Point(23, 62);
+            this.lblPrezime.Name = "lblPrezime";
+            this.lblPrezime.Size = new System.Drawing.Size(44, 13);
+            this.lblPrezime.TabIndex = 1;
+            this.lblPrezime.Text = "Prezime";
+            // 
+            // lblKorisnickoIme
+            // 
+            this.lblKorisnickoIme.AutoSize = true;
+            this.lblKorisnickoIme.Location = new System.Drawing.Point(23, 119);
+            this.lblKorisnickoIme.Name = "lblKorisnickoIme";
+            this.lblKorisnickoIme.Size = new System.Drawing.Size(75, 13);
+            this.lblKorisnickoIme.TabIndex = 2;
+            this.lblKorisnickoIme.Text = "Korisničko ime";
+            // 
+            // lblLozinka
+            // 
+            this.lblLozinka.AutoSize = true;
+            this.lblLozinka.Location = new System.Drawing.Point(23, 150);
+            this.lblLozinka.Name = "lblLozinka";
+            this.lblLozinka.Size = new System.Drawing.Size(44, 13);
+            this.lblLozinka.TabIndex = 3;
+            this.lblLozinka.Text = "Lozinka";
+            // 
+            // lblPotvrdaLozinke
+            // 
+            this.lblPotvrdaLozinke.AutoSize = true;
+            this.lblPotvrdaLozinke.Location = new System.Drawing.Point(23, 182);
+            this.lblPotvrdaLozinke.Name = "lblPotvrdaLozinke";
+            this.lblPotvrdaLozinke.Size = new System.Drawing.Size(80, 13);
+            this.lblPotvrdaLozinke.TabIndex = 4;
+            this.lblPotvrdaLozinke.Text = "Potvrda lozinke";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(23, 212);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblAdresa
+            // 
+            this.lblAdresa.AutoSize = true;
+            this.lblAdresa.Location = new System.Drawing.Point(23, 244);
+            this.lblAdresa.Name = "lblAdresa";
+            this.lblAdresa.Size = new System.Drawing.Size(40, 13);
+            this.lblAdresa.TabIndex = 6;
+            this.lblAdresa.Text = "Adresa";
+            // 
+            // lblStudent
+            // 
+            this.lblStudent.AutoSize = true;
+            this.lblStudent.Location = new System.Drawing.Point(23, 277);
+            this.lblStudent.Name = "lblStudent";
+            this.lblStudent.Size = new System.Drawing.Size(44, 13);
+            this.lblStudent.TabIndex = 7;
+            this.lblStudent.Text = "Student";
+            // 
+            // txtBoxIme
+            // 
+            this.txtBoxIme.Location = new System.Drawing.Point(109, 26);
+            this.txtBoxIme.Name = "txtBoxIme";
+            this.txtBoxIme.Size = new System.Drawing.Size(205, 20);
+            this.txtBoxIme.TabIndex = 8;
+            // 
+            // txtBoxPrezime
+            // 
+            this.txtBoxPrezime.Location = new System.Drawing.Point(109, 59);
+            this.txtBoxPrezime.Name = "txtBoxPrezime";
+            this.txtBoxPrezime.Size = new System.Drawing.Size(205, 20);
+            this.txtBoxPrezime.TabIndex = 9;
+            // 
+            // txtBoxRegKorIme
+            // 
+            this.txtBoxRegKorIme.Location = new System.Drawing.Point(109, 116);
+            this.txtBoxRegKorIme.Name = "txtBoxRegKorIme";
+            this.txtBoxRegKorIme.Size = new System.Drawing.Size(205, 20);
+            this.txtBoxRegKorIme.TabIndex = 10;
+            // 
+            // txtBoxRegLozinka
+            // 
+            this.txtBoxRegLozinka.Location = new System.Drawing.Point(109, 147);
+            this.txtBoxRegLozinka.Name = "txtBoxRegLozinka";
+            this.txtBoxRegLozinka.Size = new System.Drawing.Size(205, 20);
+            this.txtBoxRegLozinka.TabIndex = 11;
+            // 
+            // txtBoxPotvrdaLozinke
+            // 
+            this.txtBoxPotvrdaLozinke.Location = new System.Drawing.Point(109, 179);
+            this.txtBoxPotvrdaLozinke.Name = "txtBoxPotvrdaLozinke";
+            this.txtBoxPotvrdaLozinke.Size = new System.Drawing.Size(205, 20);
+            this.txtBoxPotvrdaLozinke.TabIndex = 12;
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(109, 209);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(205, 20);
+            this.txtBoxEmail.TabIndex = 13;
+            // 
+            // txtBoxAdresa
+            // 
+            this.txtBoxAdresa.Location = new System.Drawing.Point(109, 241);
+            this.txtBoxAdresa.Name = "txtBoxAdresa";
+            this.txtBoxAdresa.Size = new System.Drawing.Size(205, 20);
+            this.txtBoxAdresa.TabIndex = 14;
+            // 
+            // radioBtnDa
+            // 
+            this.radioBtnDa.AutoSize = true;
+            this.radioBtnDa.Location = new System.Drawing.Point(109, 275);
+            this.radioBtnDa.Name = "radioBtnDa";
+            this.radioBtnDa.Size = new System.Drawing.Size(39, 17);
+            this.radioBtnDa.TabIndex = 15;
+            this.radioBtnDa.TabStop = true;
+            this.radioBtnDa.Text = "Da";
+            this.radioBtnDa.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnNe
+            // 
+            this.radioBtnNe.AutoSize = true;
+            this.radioBtnNe.Location = new System.Drawing.Point(163, 275);
+            this.radioBtnNe.Name = "radioBtnNe";
+            this.radioBtnNe.Size = new System.Drawing.Size(39, 17);
+            this.radioBtnNe.TabIndex = 16;
+            this.radioBtnNe.TabStop = true;
+            this.radioBtnNe.Text = "Ne";
+            this.radioBtnNe.UseVisualStyleBackColor = true;
+            // 
+            // btnRegRegistracija
+            // 
+            this.btnRegRegistracija.Location = new System.Drawing.Point(150, 307);
+            this.btnRegRegistracija.Name = "btnRegRegistracija";
+            this.btnRegRegistracija.Size = new System.Drawing.Size(123, 23);
+            this.btnRegRegistracija.TabIndex = 17;
+            this.btnRegRegistracija.Text = "Registracija";
+            this.btnRegRegistracija.UseVisualStyleBackColor = true;
+            this.btnRegRegistracija.Click += new System.EventHandler(this.btnRegRegistracija_Click);
+            // 
+            // lblOIB
+            // 
+            this.lblOIB.AutoSize = true;
+            this.lblOIB.Location = new System.Drawing.Point(23, 91);
+            this.lblOIB.Name = "lblOIB";
+            this.lblOIB.Size = new System.Drawing.Size(25, 13);
+            this.lblOIB.TabIndex = 18;
+            this.lblOIB.Text = "OIB";
+            // 
+            // txtBoxOIB
+            // 
+            this.txtBoxOIB.Location = new System.Drawing.Point(109, 88);
+            this.txtBoxOIB.Name = "txtBoxOIB";
+            this.txtBoxOIB.Size = new System.Drawing.Size(205, 20);
+            this.txtBoxOIB.TabIndex = 19;
+            // 
+            // RegistracijaForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 372);
+            this.Controls.Add(this.txtBoxOIB);
+            this.Controls.Add(this.lblOIB);
+            this.Controls.Add(this.btnRegRegistracija);
+            this.Controls.Add(this.radioBtnNe);
+            this.Controls.Add(this.radioBtnDa);
+            this.Controls.Add(this.txtBoxAdresa);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.txtBoxPotvrdaLozinke);
+            this.Controls.Add(this.txtBoxRegLozinka);
+            this.Controls.Add(this.txtBoxRegKorIme);
+            this.Controls.Add(this.txtBoxPrezime);
+            this.Controls.Add(this.txtBoxIme);
+            this.Controls.Add(this.lblStudent);
+            this.Controls.Add(this.lblAdresa);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPotvrdaLozinke);
+            this.Controls.Add(this.lblLozinka);
+            this.Controls.Add(this.lblKorisnickoIme);
+            this.Controls.Add(this.lblPrezime);
+            this.Controls.Add(this.lblIme);
+            this.Name = "RegistracijaForma";
+            this.Text = "RegistracijaForma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIme;
+        private System.Windows.Forms.Label lblPrezime;
+        private System.Windows.Forms.Label lblKorisnickoIme;
+        private System.Windows.Forms.Label lblLozinka;
+        private System.Windows.Forms.Label lblPotvrdaLozinke;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblAdresa;
+        private System.Windows.Forms.Label lblStudent;
+        private System.Windows.Forms.TextBox txtBoxIme;
+        private System.Windows.Forms.TextBox txtBoxPrezime;
+        private System.Windows.Forms.TextBox txtBoxRegKorIme;
+        private System.Windows.Forms.TextBox txtBoxRegLozinka;
+        private System.Windows.Forms.TextBox txtBoxPotvrdaLozinke;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.TextBox txtBoxAdresa;
+        private System.Windows.Forms.RadioButton radioBtnDa;
+        private System.Windows.Forms.RadioButton radioBtnNe;
+        private System.Windows.Forms.Button btnRegRegistracija;
+        private System.Windows.Forms.Label lblOIB;
+        private System.Windows.Forms.TextBox txtBoxOIB;
+    }
+}
---
+++ Projekt/eNarudžba/eNarudžba/eNarudžba/PocetnaForma.cs
@@ -0,0 +1,34 @@
+﻿using eNarudžba.Forme;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eNarudžba
+{
+    public partial class PocetnaForma : Form
+    {
+        
+        public PocetnaForma()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrijava_Click(object sender, EventArgs e)
+        {
+            PrijavaForma prijavaForma = new PrijavaForma();
+            prijavaForma.Show();
+        }
+
+        private void btnRegistracija_Click(object sender, EventArgs e)
+        {
+            RegistracijaForma registracijaForma = new RegistracijaForma();
+            registracijaForma.Show();
+        }
+    }
+}
